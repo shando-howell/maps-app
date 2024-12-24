0cc80408e30099b16581788007d54d8cf96790b8
shandohowell <shandohowell94@gmail.com>
2024-12-23 21:46:44 -0500

    Update for Maps app

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 /// <reference types="@types/google.maps" />
-// import { User } from "./User";
-// import { Company } from "./Company"; 
+import { User } from "./User";
+import { Company } from "./Company"; 
 import { CustomMap } from "./CustomMaps";
 
-new CustomMap('map');
+const user = new User();
+const company = new Company();
+const customMap = new CustomMap('map');
+
+customMap.addMarker(user);
+customMap.addMarker(company);
+
